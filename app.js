1563f19b343d79976bf8fb3606cd09ffab5e40bf
pratik Deo <pdeo1@stevens.edu>
2020-08-07 17:24:37 -0400

    shortening text on home page & implememnting templating for posts using read more on home page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var lo = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -53,13 +54,17 @@ app.post("/compose", function(req,res){
   res.redirect("/");
 });
 
-// Route for dynamic posts pages 
+// Route for dynamic posts pages
 app.get("/posts/:day", function(req, res){
-
-    if (req.params.day === "Test") {
-      console.log("Match Found!");
+  var i = lo.lowerCase(req.params.day);
+  posts.forEach(function(ele) {
+    if (lo.lowerCase(req.params.day) === lo.lowerCase(ele.postTitle)) {
+      res.render("post", {
+        Title: ele.postTitle,
+        para: ele.textbody
+      });
     }
-
+  });
 });
 
 app.listen(3000, function() {
